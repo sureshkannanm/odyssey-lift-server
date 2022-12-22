ba51598e46cdb11fded4db2dccae4da0fad9706b
Sureshkannan <8393993+sureshkannanm@users.noreply.github.com>
2022-12-21 22:19:30 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ async function startApolloServer(typeDefs, resolvers) {
   });
 
   const { url, port } = await server.listen({port: process.env.PORT || 4000});
-  console.log(`
+  console.log(` 
       🚀  Server is running
       🔉  Listening on port ${port}
       📭  Query at ${url}
